1523e931f2b7cf7576b540991b2b00397d0e0af2
ChoeYeongjae <36364132+ChoeYeongjae@users.noreply.github.com>
2018-02-14 23:11:18 +0900

    계산기완료
    계산기완료!!!!!!!!우오오오

---
+++ function.js
@@ -0,0 +1,101 @@
+var res;
+//매개변수를이용
+function compute(f) {
+    //연산자 함수이용
+    res            = eval(f.text.value);
+    //결과값 도출
+    f.result.value += res;
+}
+function compute1(f) {
+
+    var res = 1;
+
+    f.text.value += res;
+}
+function compute2(f) {
+
+    var res = 2;
+
+    f.text.value += res;
+}
+function compute3(f) {
+
+    var res = 3;
+
+    f.text.value += res;
+}
+function compute5(f) {
+
+    var res = 4;
+
+    f.text.value += res;
+}
+
+function compute6(f) {
+
+    var res = 5;
+
+    f.text.value += res;
+}
+function compute7(f) {
+
+    var res = 6;
+
+    f.text.value += res;
+}
+
+function compute9(f) {
+
+    var res = 7;
+
+    f.text.value += res;
+}
+function compute10(f) {
+
+    var res = 8;
+
+    f.text.value += res;
+}
+function compute11(f) {
+
+    var res = 9;
+
+    f.text.value += res;
+}
+function compute13(f) {
+
+    var res = 0;
+
+    f.text.value += res;
+}
+
+function compute4(f) {
+
+    var res = "+";
+
+    f.text.value += res;
+}
+function compute8(f) {
+
+    var res = "-";
+
+    f.text.value += res;
+}
+function compute12(f) {
+
+    var res = "*";
+
+    f.text.value += res;
+}
+function compute14(f) {
+
+    var res = "%";
+
+    f.text.value += res;
+}
+function compute15(f) {
+
+    var res = "/";
+
+    f.text.value += res;
+}
