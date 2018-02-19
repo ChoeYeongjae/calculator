b78fbd9a620641a9d6eba5bbf3f487e3216b72de
ChoeYeongjae <36364132+ChoeYeongjae@users.noreply.github.com>
2018-02-19 22:56:18 +0900

    Javascript 수정완료

--- function.js
+++ function.js
@@ -3,103 +3,10 @@ var res;
 function compute(f){
  //연산자 함수이용
 
- res = eval(f.text.value);
- f.result.value = res;
- f.text.value = "";
+     res = eval(cal.text.value);
+     cal.result.value = res;
+     cal.text.value = " " ;
 }
 function compute1(f){
-
- var res = 1;
-
-  f.text.value += res;
-}
-function compute2(f){
-
- var res = 2;
-
-  f.text.value += res;
-}
-function compute3(f){
-
- var res = 3;
-
-  f.text.value += res;
-}
-function compute5(f){
-
- var res = 4;
-
-  f.text.value += res;
-}
-
-
-
-function compute6(f){
-
- var res = 5;
-
-  f.text.value += res;
-}
-function compute7(f){
-
- var res = 6;
-
-  f.text.value += res;
-}
-
-function compute9(f){
-
- var res = 7;
-
-  f.text.value += res;
-}
-function compute10(f){
-
- var res = 8;
-
-  f.text.value += res;
-}
-function compute11(f){
-
- var res = 9;
-
-  f.text.value += res;
-}
-function compute13(f){
-
- var res = 0;
-
-  f.text.value += res;
-}
-
-
-function compute4(f){
-
- var res = "+";
-
-  f.text.value += res;
-}
-function compute8(f){
-
- var res = "-";
-
-  f.text.value += res;
-}
-function compute12(f){
-
- var res = "*";
-
-  f.text.value += res;
-}
-function compute14(f){
-
- var res = "%";
-
-  f.text.value += res;
-}
-function compute15(f){
-
- var res = "/";
-
-  f.text.value += res;
+  cal.text.value += f;
 }
